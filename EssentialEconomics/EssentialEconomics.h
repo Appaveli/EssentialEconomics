647b5247f5e1b4cb32af8dd89c698834db7fabdd
Dominque <bigapplezone@bigapplezones-MacBook-Pro.local>
2014-09-26 18:26:46 -0700

    Initial Commit

---
+++ EssentialEconomics/EssentialEconomics.h
@@ -0,0 +1,13 @@
+//
+//  EssentialEconomics.h
+//  EssentialEconomics
+//
+//  Created by Dominque on 9/26/14.
+//  Copyright (c) 2014 Dominque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EssentialEconomics : NSObject
+
+@end
---
+++ EssentialEconomics/EssentialEconomics.m
@@ -0,0 +1,13 @@
+//
+//  EssentialEconomics.m
+//  EssentialEconomics
+//
+//  Created by Dominque on 9/26/14.
+//  Copyright (c) 2014 Dominque. All rights reserved.
+//
+
+#import "EssentialEconomics.h"
+
+@implementation EssentialEconomics
+
+@end
